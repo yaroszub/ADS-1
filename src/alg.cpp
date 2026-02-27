d01e29d93fea3327f1cca0333df92f5ab497318a
yaroszub <yaroslav.zubkov.2020@bk.ru>
2026-02-27 20:37:35 +0300

    Зубков Ярослав 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,54 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+    if (value <2) return false;
+  if (value %2==0) return value ==2;
+  for (int i =2;i*i<=value;++i)
+  if (value % i ==0)
+   return false;
   return true;
 }
-
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n ==0) return 0;
+  uint64_t a = 0;
+  uint64_t b = 1;
+  while (a < n) {
+    b++;
+    if (checkPrime(b)){
+      a++;
+    }
+  }
+  return b;
 }
-
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t cancan = value + 1;
+  while (!checkPrime(cancan)){
+    cancan++;
+  }
+  return cancan;
 }
-
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  uint64_t c=2;
+  while (c < hbound){
+  sum += c;
+    c = nextPrime(c);
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+uint64_t schet = 0;
+uint64_t d = lbound;
+while(!checkPrime(d)&& p <= hbound){
+  d++;
+}
+  while(d <= hbound) {
+    uint64_t f = nextPrime(d);
+    if (f <= hbound && f - d == 2){
+       schet++ ;
+    }
+    d = f;
+        }
+  return schet;
 }
